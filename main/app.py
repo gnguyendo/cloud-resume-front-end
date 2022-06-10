a718c3fc9203dc608a97c66600a127cba5a377e9
geoffrey <gnguyendo@gmail.com>
2022-06-10 14:13:05 -0700

    removed dyanmodb from lambda code

--- main/app.py
+++ main/app.py
@@ -2,30 +2,30 @@ import boto3
 
 
 def test_lambda(event, context):
-    # Get the service resource.
-    dynamodb = boto3.resource('dynamodb')
-
-    # Create the DynamoDB table.
-    table = dynamodb.create_table(
-        TableName='viewcount',
-        KeySchema=[
-            {
-                'AttributeName': 'WebPageViews',
-                'KeyType': 'HASH'
-            },
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'WebPageViews',
-                'AttributeType': 'N'
-            }
-        ]
-    )
-    # Wait until the table exists.
-    table.wait_until_exists()
-
-    # Print out some data about the table.
-    print(table.item_count)
+    # # Get the service resource.
+    # dynamodb = boto3.resource('dynamodb')
+    #
+    # # Create the DynamoDB table.
+    # table = dynamodb.create_table(
+    #     TableName='viewcount',
+    #     KeySchema=[
+    #         {
+    #             'AttributeName': 'WebPageViews',
+    #             'KeyType': 'HASH'
+    #         },
+    #     ],
+    #     AttributeDefinitions=[
+    #         {
+    #             'AttributeName': 'WebPageViews',
+    #             'AttributeType': 'N'
+    #         }
+    #     ]
+    # )
+    # # Wait until the table exists.
+    # table.wait_until_exists()
+    #
+    # # Print out some data about the table.
+    # print(table.item_count)
 
     message = "Hello Lambda World"
     return message
