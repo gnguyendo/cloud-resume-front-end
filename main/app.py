af27e7b05112ecc43bd8449f0a7f45f85e71e031
geoffrey <gnguyendo@gmail.com>
2022-06-27 13:04:55 -0700

    updated lambda json response

--- main/app.py
+++ main/app.py
@@ -8,9 +8,8 @@ web_page_counter = os.environ.get('DataBaseName')
 curr_date = date.today().strftime("%m/%d/%y")
 
 
-
 def test_lambda(event, context):
-    response = client.update_item(
+    response_body = client.update_item(
         TableName=web_page_counter,
         Key={"Date": {"S": curr_date}},
         UpdateExpression="ADD total_views :view",
@@ -18,27 +17,15 @@ def test_lambda(event, context):
         ReturnValues="ALL_NEW"
     )
     d = {1: 'a', 2: 'b', 3: 'c'}
-    json_object = json.dumps(response, indent=2)
+    # json_object = json.dumps(response, indent=2)
 
-    book_json = \
+    response = \
         {
-           "book": [
-
-              {
-                 "id":"01",
-                 "language": "Java",
-                 "edition": "third",
-                 "author": "Herbert Schildt"
-              },
-
-              {
-                 "id":"07",
-                 "language": "C++",
-                 "edition": "second",
-                 "author": "E.Balagurusamy"
-              }
-           ]
+            "isBase64Encoded": True,
+            "statusCode": 200,
+            # "headers": {"headerName": "headerValue"},
+            # "multiValueHeaders": {},
+            "body": json.dumps(response_body, indent=2)
         }
 
-    test_json = json.dumps(book_json, indent=2)
-    return test_json
+    return response
